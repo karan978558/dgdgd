4de7494b86d3346ceeb1c67d58434c479e8242a8
karan978558 <karannagar9785@gmail.com>
2024-08-13 10:12:59 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,422 @@
+// let hindi = Number(prompt("Enter five number"));
+// let english = Number(prompt("Enter five number"));
+// let maths = Number(prompt("Enter five number"));
+// let science = Number(prompt("Enter five number"));
+// let social_science = Number(prompt("Enter five number"));
+// let total = (hindi+ english +maths +science + social_science);
+
+
+//   let a = Number(prompt("Enter a number"));
+//   if(a %3 == 0 && a%5 == 0){
+//       document.write("YES This Number is fully divided by both 3 and 5");
+
+//   }
+//   else{
+//     document.write("NO");
+
+//   }
+// let a = Number(prompt("Enter a number"));
+// if(a == 1){
+//     document.write("SUNDAY");
+// }
+// else if(a == 2){
+//     document.write("MONDAY");
+// }
+// else if(a == 3){
+//     document.write("TUESDAY");
+// }
+// else if(a == 4){
+//     document.write("WEDNESDAY");
+// }
+// else if(a == 5){
+//     document.write("THURSDAY");
+// }
+// else if(a == 6){
+//     document.write("FRIDAY");
+// }
+// else if(a == 7){
+//     document.write("SATURDAY");
+// }
+// else{
+//     document.write("INVALID NUMBER");
+// } 
+
+// let  a = String(prompt("Enter a fruit name"));
+// if(a == "banana" ||a == "apple" ||a == "mango"){
+//     document.write("FRUIT");
+// }
+// else{
+//     document.write("NOT a fruit");
+// }
+
+// let x =  Number(prompt("Enter first number"));
+// let y =  Number(prompt("Enter second number"));
+// if(x>0 || y>15){
+//     document.write("RIGHT");
+// }
+// else{
+//     document.write("WRONG");
+// }
+
+// let x =  Number(prompt("Enter first number"));
+// let y =  Number(prompt("Enter second number"));
+// let z =  Number(prompt("Enter third number"));
+// if(x>y && x>z){
+//     document.write(x + "is greater than" +y , "</br>"+ z);
+// }
+// else if(y>x && y>z){
+//     document.write(y+ "is greater than" +x , "</br>"+z);
+
+// }
+// else{
+//     document.write(z+ "is greater than" +x  , "</br>"+y);
+// }
+
+// let x =  Number(prompt("Enter length of x"));
+// let y =  Number(prompt("Enter length of y"));
+// let z =  Number(prompt("Enter length of z"));
+// if(x==y || x==z){
+//     document.write("ISOSCALES TRIANGLE");
+// }
+// else if(x=y=z){
+//     document.write("EQUILATERALTRIANGLE");
+// }
+// else if(x!=y!=z){
+//     document.write("Scales triangle");
+// }
+
+// if(x==y && x==z){
+//     document.write("ISOSCALES TRIANGLE");
+// }
+
+// let x = String (prompt("Enter a alphabet"));
+// if(x == "a"||x =="A"||x == "e"||x =="E"||x == "i"||x == "I"||x =="o"||x == "O"||x == "u"|| x == "U"){
+//     document.write("Vowel");
+// }
+// else{
+//     document.write("consonent");
+// }
+
+
+// let age =Number (prompt("Enter your age"));
+// let licence = Boolean (prompt("Enter a licence"));
+// if(age>=18){
+//     if(licence = true){
+//         document.write("you are allow for driving");
+//     }
+//     else{
+//         document.write("Sorry not allow");
+//     }
+// }
+// else{
+//     document.write("apply for licence");
+
+// }
+
+
+
+// let mark1 =Number(prompt("Enter your mark1"));
+// let mark2 =Number(prompt("Enter your mark2"));
+// let mark3 =Number(prompt("Enter your mark3"));
+// let mark4 =Number(prompt("Enter your mark4"));
+// let mark5 =Number(prompt("Enter your mark5"));
+// let total =(mark1+mark2+mark3+mark4+mark5);
+// let marks=(total/500)*100
+// if(marks >= 90 && marks <= 100){
+//     document.write("A grade")
+// }
+// else{
+//     if(marks >= 80 && marks >=90){
+//         document.write("B grade");
+//     }
+//     else{
+//         if(marks)
+//     }
+// }
+
+
+// let temp =Number(prompt("Enter your temp"));
+// let session =Boolean(prompt("Enter your session"));
+// if(temp>25){
+//     if(raining == true){
+//         document.write("heatweave  but raining");
+//     }
+//     else{
+//         document.write("heatweve but not raining");
+//     }
+    
+// }
+// else 
+// {
+//     document.write("colday");
+// }
+
+
+// let user =String(prompt("Enter your username"));
+
+
+// if(user == "Nandugupta")
+
+// {
+//     let password =Number(prompt("Enter your password"));
+//     if(password == 12345678){
+//         document.write("successfuly login")
+//     }
+//     else{
+//         document.write("incorrect password");
+//     }
+// }
+// else{
+//     document.write("incorrect username");
+// }
+
+
+// let num =Number(prompt("Enter a number"));
+
+//     if(num >0){
+     
+//         if(num%2 == 0){
+//             document.write("Even number and positive");
+
+//     }
+//     else{
+//         document.write("odd number but positive");
+//     }
+
+// }
+// else{
+//   document.write("even but negative")
+    
+//     if(num%3 == 0){
+//         document.write("odd number and negative");
+//     }
+//     else{
+//         document.write("even number")
+//     }  
+// }
+
+
+// let username =String(prompt("Enter username"));
+// let email =String(prompt("Enter email"));
+// let password =Boolean(prompt("Enter your password"));
+
+// if(username == "Nandugupta"){
+//     if(email == "guptanandu675")
+//     {
+//         {
+//         if(password == 12345678) 
+//         {
+//             document.write("Fome submit");
+//         }
+//         else{
+//             document.write("invalid password");
+//         }
+       
+//         }
+//     else{
+//         document.write("invalid mail");
+//     }
+    
+   
+//     }
+// else{
+//     document.write("Sorry please enter username");
+// }
+
+// let role= "editor";
+// switch(role){
+//     case "admin":
+//     document.write("you have fully access");
+//     break;
+
+//     case "editor":
+//     document.write("you can edit document");
+//     break; 
+
+//     case "viewer":
+//     document.write("you can view document");
+//     break;
+
+//     default :
+//     document.write("you cannot access document");
+    
+
+
+// }
+
+
+// let trafic =String(prompt("Enter a color"));
+// switch(trafic){
+//     case "red":
+//         document.write("stop please");
+//         break;
+
+//     case "yellow":
+//         document.write("stop inside");
+//         break; 
+        
+//     case "green":
+//         document.write("lets go");
+//         break; 
+        
+//       default:
+//             document.write("soory not found");
+              
+               
+
+                
+        
+// }
+
+
+// let week =String(prompt("Enter a value"));
+// switch(week){
+//     case "1":
+//         document.write("JAN");
+//         break;
+
+//     case "2":
+//         document.write("FEB");
+//         break; 
+        
+//     case "3":
+//         document.write("MAR");
+//         break; 
+//     case "4":
+//         document.write("APR");
+//         break; 
+//      case "5":
+//         document.write("MAY");
+//         break;
+//      case "6":
+//         document.write("JUN");
+//         break; 
+//     case "7":
+//         document.write("JUL");
+//         break;  
+//     case "8":
+//         document.write("AUG");
+//         break; 
+//     case "9":
+//         document.write("SEP");
+//         break; 
+//     case "10":
+//         document.write("OCT");
+//         break; 
+//     case "11":
+//         document.write("NOV");
+//         break; 
+//     case "12":
+//         document.write("DEC");
+//         break; 
+        
+//     default:
+//         document.write("soory not found");
+
+// }   
+
+
+// let mark1 =parseFloat(prompt("Enter a mark1"));
+// let mark2 =parseFloat(prompt("Enter a mark2"));
+// let mark3 =parseFloat(prompt("Enter a mark3"));
+// let mark4 =parseFloat(prompt("Enter a mark4"));
+// let mark5 =parseFloat(prompt("Enter a mark5"));
+// let sum=mark1+mark2+mark3+mark4+mark5;
+// let total=(sum/500)*100;
+// switch(true){
+//     case (total>=90  && total <=100 ):
+//         document.write("A grade");
+//         break;
+
+//     case (total >=80  && total <=90) :
+//         document.write("B grade");
+//         break; 
+        
+//     case  (total >=70  && total <=80):
+//         document.write("C grade");
+//         break; 
+//     case ( total >=60  && total <=70):
+//         document.write("D grade");
+//         break; 
+//      case  (total >=50  && total <=60):
+//         document.write("E grade");
+//         break;
+
+//      default :
+//      document.write("fail");   
+// }
+
+// let num1 =parseFloat(prompt("Enter a num1"));
+// let operator =(prompt("Enter operato"));
+// let num2 =parseFloat(prompt("Enter a num2"));
+
+// switch(operator){
+//     case "+":
+//     document.write(num1+num2);
+//     break;
+//     case "-":
+//     document.write(num1-num2);
+//     break;
+//     case "*":
+//     document.write(num1*num2);
+//     break;
+//     case "/":
+//     document.write(num1/num2);
+//     break;
+//     case "%":
+//     document.write(num1%num2);
+//     break;
+
+//     default :
+//     document.write("sorry not found");
+// }
+// let a;
+// for( a = 1; a <= 5;a++){
+//     document.write(a,"</br>");
+// }
+// let sum = 0;
+// let
+// for(let i = 5; i<=51; i=i+5){
+//     // if(i%5==0){
+//     //    sum = sum +i;
+//     // }
+//     document.write()
+//     document.write(i,"</br>"); 
+    
+// }
+
+
+
+
+// let i;
+
+// for(let i=1;i<=10;i++)
+// {
+//     document.write(a);
+// }
+
+
+
+
+// for( let i =1; i<=10;i++){
+
+//     // document.write("1*1");
+//     document.write(i,"*",i,"=",i*i,"</br>");
+// }
+
+
+// let a = ("javascript");
+// let b ="";
+// for(let a = a.length - 1;a>=0;a--){
+//     b+=a[a];
+
+
+// }
+// console.log(b);
+
+
+// let a =1;
+// while(a<=10){
+//     document.write(a);
+//     a++;
+// }
